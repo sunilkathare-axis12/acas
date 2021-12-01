bb74a47c4e15b6c18c79af8f32cf02fd033f4519
sunilkathare-axis12 <93491916+sunilkathare-axis12@users.noreply.github.com>
2021-12-01 15:50:02 +0000

    draft prototype 1

--- app/routes.js
+++ app/routes.js
@@ -36,4 +36,55 @@ router.post('/page-one-email', function (req, res) {
   }
 
 })
+
+// Run this code when a form is submitted to 'juggling-balls-answer'
+router.post('/who-are-you-representing', function (req, res) {
+
+  // Make a variable and give it the value from 'how-many-balls'
+  var representing = req.session.data['representing']
+
+  // Check whether the variable matches a condition
+  if (representing == "myself"){
+    // Send user to next page
+    res.redirect('/a-02-represent-myself')
+  } else {
+    // Send user to ineligible page
+    res.redirect('/a-02-represent-others')
+  }
+
+})
+
+// Run this code when a form is submitted to 'try ec'
+router.post('/try-ec', function (req, res) {
+
+  // Make a variable and give it the value from 'how-many-balls'
+  var tryec = req.session.data['try-ec']
+
+  // Check whether the variable matches a condition
+  if (tryec == "yes"){
+    // Send user to next page
+    res.redirect('/page-one')
+  } else {
+    // Send user to ineligible page
+    res.redirect('/a-no-ec-contact')
+  }
+
+})
+
+// Run this code when a form is submitted to 'try ec'
+router.post('/rep-others', function (req, res) {
+
+  // Make a variable and give it the value from 'how-many-balls'
+  var reprelationship = req.session.data['rep-relationship']
+
+  // Check whether the variable matches a condition
+  if (reprelationship == "family-friend"){
+    // Send user to next page
+    res.redirect('/a-02-represent-others-01')
+  } else {
+    // Send user to ineligible page
+    res.redirect('/a-02-represent-others-01')
+  }
+
+})
 module.exports = router
